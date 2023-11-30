584bd0f4c2afea38169da9d23db42dfc2ba5903a
blade <blade@nodies.org>
2023-11-30 08:20:36 -0600

    remove unused funcs

--- pkg/pokt/pokt_v0/models/session.go
+++ pkg/pokt/pokt_v0/models/session.go
@@ -1,8 +1,6 @@
 //go:generate ffjson $GOFILE
 package models
 
-import "math/rand"
-
 type Node struct {
 	ServiceUrl string `json:"service_url"`
 	PublicKey  string `json:"public_key"`
@@ -25,22 +23,4 @@ type GetSessionRequest struct {
 	AppPubKey string `json:"app_public_key"`
 	Chain     string `json:"chain"`
 	Height    uint   `json:"session_height"`
-}
-
-func FindNodeFromPubKey(nodes []*Node, pubKey string) *Node {
-	for _, n := range nodes {
-		if n.PublicKey == pubKey {
-			return n
-		}
-	}
-	return nil
-}
-
-func GetRandomNode(nodes []*Node) *Node {
-	if len(nodes) == 0 {
-		return nil
-	}
-	randomIndex := rand.Intn(len(nodes))
-
-	return nodes[randomIndex]
 }
