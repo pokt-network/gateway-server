d0ecc9457bb37d88ff287350fc1c9bb3cd079517
nodiesBlade <blade@nodies.org>
2023-12-18 03:06:59 -0600

    add more tests!

--- pkg/pokt/pokt_v0/generate_proof_bytes_test.go
+++ pkg/pokt/pokt_v0/generate_proof_bytes_test.go
@@ -7,24 +7,34 @@ import (
 )
 
 func Test_generateRelayProof(t *testing.T) {
-	type relayProofArgs struct {
-		entropy        uint64
-		chainId        string
-		sessionHeight  uint
-		servicerPubKey string
-		relayMetadata  *models.RelayMeta
-		reqPayload     *models.Payload
-		account        *models.Ed25519Account
-	}
-	tests := []struct {
-		name string
-		args relayProofArgs
-		want *models.RelayProof
-	}{}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equal(t, generateRelayProof(tt.args.entropy, tt.args.chainId, tt.args.sessionHeight, tt.args.servicerPubKey, tt.args.relayMetadata, tt.args.reqPayload, tt.args.account), tt.want)
-		})
+	account, err := models.NewAccount("3fe64039816c44e8872e4ef981725b968422e3d49e95a1eb800707591df30fe374039dbe881dd2744e2e0c469cc2241e1e45f14af6975dd89079d22938377849")
+	assert.Equal(t, err, nil)
+
+	chainId := "0001"
+	sessionHeight := uint(1)
+	servicerPubKey := "0x"
+	relayMetadata := &models.RelayMeta{BlockHeight: sessionHeight}
+	requestPayload := &models.Payload{
+		Data:    "randomJsonPayload",
+		Method:  "post",
+		Path:    "",
+		Headers: nil,
 	}
+	entropy := uint64(1)
+	assert.Equal(t, generateRelayProof(entropy, chainId, sessionHeight, servicerPubKey, relayMetadata, requestPayload, account), &models.RelayProof{
+		Entropy:            1,
+		SessionBlockHeight: 1,
+		ServicerPubKey:     "0x",
+		Blockchain:         "0001",
+		AAT: &models.AAT{
+			Version:      "0.0.1",
+			AppPubKey:    "74039dbe881dd2744e2e0c469cc2241e1e45f14af6975dd89079d22938377849",
+			ClientPubKey: "74039dbe881dd2744e2e0c469cc2241e1e45f14af6975dd89079d22938377849",
+			Signature:    "f233ca857b4ada2ca4996e0da8c1761cfbc855edf282fc5a753d4631785946d6c2b08c781c84abbca2dc929de50008729079124e5c5c16921a81139279020a05",
+		},
+		Signature:   "befcc42130fb9e46fb9874acfb5bd8a9f783db60f86d1b1eb61cdba23fdb7e9e17544cb99afb480c9e1308532e07cdf6f4e2da27790f47dae30133725191b309",
+		RequestHash: "c5b64f9a7901ed8c3341f7440913a5ddd7b694dc7b4daeb234a47a9c42b653bb",
+	})
+
 }
--- pkg/pokt/pokt_v0/get_session_from_request_test.go
+++ pkg/pokt/pokt_v0/get_session_from_request_test.go
@@ -1,32 +1,84 @@
 package pokt_v0
 
 import (
-	"fmt"
+	"errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"os-gateway/mocks"
 	"os-gateway/pkg/pokt/pokt_v0/models"
 	"testing"
 )
 
 func TestGetSessionFromRequest(t *testing.T) {
+
+	mockSession := &models.Session{
+		Nodes: []*models.Node{
+			{
+				ServiceUrl: "example-node-1",
+				PublicKey:  "example-pub-key-1",
+			},
+		},
+		SessionHeader: &models.SessionHeader{
+			SessionHeight: uint(1),
+		},
+	}
+
+	mockErr := errors.New("failure")
+
 	type args struct {
 		pocketService PocketService
 		req           *models.SendRelayRequest
 	}
 	tests := []struct {
-		name    string
-		args    args
-		want    *models.Session
-		wantErr assert.ErrorAssertionFunc
+		name            string
+		generateArgs    func() args
+		expectedSession *models.Session
+		expectedErr     error
 	}{
-		// TODO: Add test cases.
+		{
+			name: "SessionFromInnerRequest",
+			generateArgs: func() args {
+				return args{
+					pocketService: nil,
+					req:           &models.SendRelayRequest{Session: mockSession},
+				}
+			},
+			expectedErr:     nil,
+			expectedSession: mockSession,
+		},
+		{
+			name: "SessionFromPocketServiceError",
+			generateArgs: func() args {
+				mockPocketService := new(mocks.PocketService)
+				mockPocketService.EXPECT().GetSession(mock.Anything).Return(nil, mockErr).Times(1)
+				return args{
+					pocketService: mockPocketService,
+					req:           &models.SendRelayRequest{Session: nil, Signer: &models.Ed25519Account{}},
+				}
+			},
+			expectedErr:     mockErr,
+			expectedSession: nil,
+		},
+		{
+			name: "SessionFromPocketServiceSuccess",
+			generateArgs: func() args {
+				mockPocketService := new(mocks.PocketService)
+				mockPocketService.EXPECT().GetSession(mock.Anything).Return(&models.GetSessionResponse{Session: mockSession}, nil).Times(1)
+				return args{
+					pocketService: mockPocketService,
+					req:           &models.SendRelayRequest{Session: nil, Signer: &models.Ed25519Account{}},
+				}
+			},
+			expectedErr:     nil,
+			expectedSession: mockSession,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetSessionFromRequest(tt.args.pocketService, tt.args.req)
-			if !tt.wantErr(t, err, fmt.Sprintf("GetSessionFromRequest(%v, %v)", tt.args.pocketService, tt.args.req)) {
-				return
-			}
-			assert.Equalf(t, tt.want, got, "GetSessionFromRequest(%v, %v)", tt.args.pocketService, tt.args.req)
+			args := tt.generateArgs()
+			session, err := GetSessionFromRequest(args.pocketService, args.req)
+			assert.Equal(t, err, tt.expectedErr)
+			assert.Equal(t, session, tt.expectedSession)
 		})
 	}
 }
