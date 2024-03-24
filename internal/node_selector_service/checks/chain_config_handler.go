ac4c005d6dc015c751afe6f4c8aefc643c73234a
blade <blade@nodies.org>
2024-03-24 16:46:07 -0500

    update checks

--- internal/node_selector_service/checks/chain_config_handler.go
+++ internal/node_selector_service/checks/chain_config_handler.go
@@ -11,6 +11,7 @@ func getBlockHeightTolerance(chainConfiguration chain_configurations_registry.Ch
 	return int(*chainConfig.HeightCheckBlockTolerance)
 }
 
+// getDataIntegrityHeightLookback - helper function ro retrieve data integrity lookback across checks
 func getDataIntegrityHeightLookback(chainConfiguration chain_configurations_registry.ChainConfigurationsService, chainId string, defaultValue int) int {
 	chainConfig, ok := chainConfiguration.GetChainConfiguration(chainId)
 	if !ok {
