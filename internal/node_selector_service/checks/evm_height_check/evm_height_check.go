89d5e2d44b8f9e75977f3d98e1b997164bf00728
blade <blade@nodies.org>
2024-03-28 11:44:53 -0500

    remove unneeded refractors

--- internal/node_selector_service/checks/evm_height_check/evm_height_check.go
+++ internal/node_selector_service/checks/evm_height_check/evm_height_check.go
@@ -171,7 +171,7 @@ func getHighestNodeHeight(nodes []*models.QosNode) uint64 {
 		if math.Abs(zScore) > zScoreHeightThreshold {
 			continue
 		}
-		// SessionHeight is higher than last recorded height
+		// Height is higher than last recorded height
 		if nodeHeight > highestNodeHeight {
 			highestNodeHeight = nodeHeight
 		}
